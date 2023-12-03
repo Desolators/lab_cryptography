be95bca851b476ab89dd0e761e7fb72e1712af08
Desolators <saschakosow@yandex.ru>
2023-12-03 17:48:45 +0300

    Доделал словари, теперь полностью другой способ решения

--- laboratories/lab_2_ver_2.py
+++ laboratories/lab_2_ver_2.py
@@ -13,6 +13,9 @@ dict_alp = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f', 'g': 'g'
             'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z'}
 freq_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
            'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+dict_num = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12,
+            'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,
+            'z': 25}
 key = int(input("Enter key: "))
 generator = [dict(freq_dict) for i in range(key)]
 lens = [0] * key
@@ -59,19 +62,17 @@ for k in range(len(alp)):
     print(k + 1, ".", *alp[k], *x)
 key_vizener = input('Enter key: ').split()
 for k in range(len(key_vizener)):
-    index0 = ord(key_vizener[k])
-    index_current = index0 - ord("a")
-    if key_vizener[k] == alp[index_current]:
-        key_vizener[k] = index_current
+    index_current = str(key_vizener[k])
+    if key_vizener[k] == dict_alp[index_current]:
+        key_vizener[k] = dict_num[index_current]
 otvet = [""] * len(shifr)
 start = 0
 for k in range(len(shifr)):
     if start > (key - 1):
         start = 0
-    index0 = ord(shifr[k])
-    index_current = index0 - ord("a")
+    index_current = shifr[k]
     for h in range(start, len(key_vizener), 1):
-        digital_shifr = (index_current - key_vizener[h]) % 26
+        digital_shifr = (dict_num[index_current] - key_vizener[h]) % 26
         index_current = digital_shifr
         if digital_shifr == index_current:
             otvet[k] = alp[index_current]
