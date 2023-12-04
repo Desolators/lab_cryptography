8158f8e29629ed1bab44932e5404cd62634698dd
Desolators <saschakosow@yandex.ru>
2023-12-04 17:17:00 +0300

    начало

--- laboratories/kudinov.lab3.py
+++ laboratories/kudinov.lab3.py
@@ -1,13 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def rect_wave(t, period):
-    return np.where(np.logical_and(t >= -period/4, t <= period/4), 1, e)
+    return np.where(np.logical_and(t >= -period / 4, t <= period / 4), 1, 0)
+
+
 def compute_inner_product(period1, period2, duration, shift):
-    t = np.linspace(-duration/2, duration/2, 1000)
+    t = np.linspace(-duration / 2, duration / 2, 1000)
     wave1 = rect_wave(t - shift, period1)
     wave2 = rect_wave(t, period2)
     inner_product = np.sum(wave1 * wave2)
     return inner_product
+
+
 def orthogonal_rect_waves(period1, period2, duration, shift):
     inner_product = compute_inner_product(period1, period2, duration, shift)
     if np.isclose(inner_product, 0):
@@ -21,8 +27,8 @@ def orthogonal_rect_waves(period1, period2, duration, shift):
     wave2 = rect_wave(t, period2)
     plt.figure(figsize=(10, 6))
     plt.subplot(211)
-    plt.plot(t, wave1, color='blue', linestyle=linestyle, label=f'Wave 1 (period = {period1})
-    plt.plot(t, wave2, color='red', linestyle=linestyle, label=f'Wave 2 (period = {period2})
+    plt.plot(t, wave1, color='blue', linestyle=linestyle, label=f'Wave 1 (period = {period1}')
+    plt.plot(t, wave2, color='red', linestyle=linestyle, label=f'Wave 2 (period = {period2}')
     plt.xlabel('t')
     plt.ylabel('Amplitude')
     plt.title('Orthogonal Rectangular Waves with Time Shift')
@@ -38,4 +44,10 @@ def orthogonal_rect_waves(period1, period2, duration, shift):
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-
+period1 = 1
+period2 = 2
+duration = 4
+shift = 0.5
+inner_product = compute_inner_product(period1, period2, duration, shift)
+print (f'Значение интеграла произведения волн: {inner_product}')
+orthogonal_rect_waves(period1, period2, duration, shift)
