8521a0de6316d90db471cb0e3d673b2635706436
Desolators <saschakosow@yandex.ru>
2023-12-05 18:51:29 +0300

    убрал лишнию переменную в словаре, переделал названия переменных

--- laboratories/lab_2_ver_2.py
+++ laboratories/lab_2_ver_2.py
@@ -9,21 +9,20 @@ shifr = ("isfptitekthhcxatwbsipppdlmmweujgdtxhuaaxxxlelwegseedrzwegspxsfkjwawilj
          "kailxeyfeathsgactwygsvxsegsjbuxrnekswhujcxqgtzxgkhiftropxxapfnroxisylemtvnchxgegwsngegsiotrmwighsftfhscztrxge"
          "eacaphmdkhpjmtvaxq")
 alp = "abcdefghijklmnopqrstuvwxyz"
-dict_alp = {'a': ['a', 0, 0], 'b': ['b', 0, 1], 'c': ['c', 0, 2], 'd': ['d', 0, 3], 'e': ['e', 0, 4], 'f': ['f', 0, 5],
-            'g': ['g', 0, 6], 'h': ['h', 0, 7], 'i': ['i', 0, 8], 'j': ['j', 0, 9], 'k': ['k', 0, 10], 'l': ['l', 0, 11],
-    'm': ['m', 0, 12], 'n': ['n', 0, 13], 'o': ['o', 0, 14], 'p': ['p', 0, 15], 'q': ['q', 0, 16], 'r': ['r', 0, 17],
-    's': ['s', 0, 18], 't': ['t', 0, 19], 'u': ['u', 0, 20], 'v': ['v', 0, 21], 'w': ['w', 0, 22], 'x': ['x', 0, 23],
-    'y': ['y', 0, 24], 'z': ['z', 0, 25]}
+dict_alp = {'a': [0, 0], 'b': [0, 1], 'c': [0, 2], 'd': [0, 3], 'e': [0, 4], 'f': [0, 5], 'g': [0, 6], 'h': [0, 7],
+            'i': [0, 8], 'j': [0, 9], 'k': [0, 10], 'l': [0, 11], 'm': [0, 12], 'n': [0, 13], 'o': [0, 14], 'p': [0, 15],
+            'q': [0, 16], 'r': [0, 17], 's': [0, 18], 't': [0, 19], 'u': [0, 20], 'v': [0, 21], 'w': [0, 22],
+            'x': [0, 23], 'y': [0, 24], 'z': [0, 25]}
 key = int(input("Enter key: "))
-generator = [deepcopy(dict_alp) for i in range(key)]
+list_of_nested_dictionaries = [deepcopy(dict_alp) for i in range(key)]
 lens = [0] * key
 for elem in range (len(shifr)):
     column_number = elem % key
     current_element = str(shifr[elem])
-    if shifr[elem] == dict_alp[current_element][0]:
-        generator[column_number][current_element][1] += 1
+    if shifr[elem]:
+        list_of_nested_dictionaries[column_number][current_element][0] += 1
         lens[column_number] += 1
-freq = [list(generator[elem].values()) for elem in range(key)]
+freq = [list(list_of_nested_dictionaries[elem].values()) for elem in range(key)]
 count_sdvig = [[0] for i in range(key - 1)]
 umnoz_so_sdvigom = [[0] * len(dict_alp) for i in range(key)]
 for k in range(len(umnoz_so_sdvigom) - 1):
@@ -32,12 +31,12 @@ for k in range(len(umnoz_so_sdvigom) - 1):
     index = 0
     count = 0
     while index <= 0.059:
-        tmp = freq[k + 1][len(dict_alp) - 1][1]
+        tmp = freq[k + 1][len(dict_alp) - 1][0]
         for h in range(len(dict_alp) - 2, -1, -1):
-            freq[k + 1][h + 1][1] = freq[k + 1][h][1]
-        freq[k + 1][0][1] = tmp
+            freq[k + 1][h + 1][0] = freq[k + 1][h][0]
+        freq[k + 1][0][0] = tmp
         for i in range(len(dict_alp)):
-            umnoz_so_sdvigom[k][i] = (freq[0][i][1] * freq[k + 1][i][1])
+            umnoz_so_sdvigom[k][i] = (freq[0][i][0] * freq[k + 1][i][0])
         sum_sdvig = sum(umnoz_so_sdvigom[k])
         for j in range(len(lens)):
             if j > (key - 1):
@@ -60,19 +59,18 @@ for k in range(len(dict_alp)):
 key_vizener = input('Enter key: ').split()
 for k in range(len(key_vizener)):
     current_element = key_vizener[k]
-    if key_vizener[k] == dict_alp[current_element][0]:
-        key_vizener[k] = dict_alp[current_element][2]
+    if key_vizener[k]:
+        key_vizener[k] = dict_alp[current_element][1]
 otvet = [""] * len(shifr)
 start = 0
-for k in range(len(shifr)):
+for i in range(len(shifr)):
     if start > (key - 1):
         start = 0
-    current_element = shifr[k]
-    for h in range(start, len(key_vizener)):
-        digital_shifr = (dict_alp[current_element][2] - key_vizener[h]) % 26
-        current_element = digital_shifr
-        if digital_shifr == current_element:
-            otvet[k] = alp[current_element]
+    current_element = shifr[i]
+    for j in range(start, len(key_vizener)):
+        current_element = (dict_alp[current_element][1] - key_vizener[j]) % 26
+        if current_element:
+            otvet[i] = alp[current_element]
         start += 1
         break
 print("Plain text: ", *otvet, sep="")
