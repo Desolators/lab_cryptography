9e7adbad1c137b834f7e43ec6d43aaff7aca33b9
Desolators <saschakosow@yandex.ru>
2023-12-11 00:05:06 +0300

    поменял назване переменной

--- laboratories/maths_thinks.py
+++ laboratories/maths_thinks.py
@@ -5,13 +5,13 @@ mas_str(0)
 def fibonacci(n):
     if n <= 1:
         return n
-    count, result = 0, 0
+    count, n = 0, 0
     n1, n2 = 0, 1
     while count != n - 1:
-        result = n1 + n2
-        n1, n2 = n2, result
+        n = n1 + n2
+        n1, n2 = n2, n
         count += 1
-    return result
+    return n
 
 
 def fibonacci_list(n):
