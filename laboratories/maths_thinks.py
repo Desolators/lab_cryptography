2a74207380904ff18879500997ed7efe9766ad72
Desolators <saschakosow@yandex.ru>
2023-12-11 00:02:57 +0300

    Фибаначи с сохранением всех результатов и без

--- laboratories/maths_thinks.py
+++ laboratories/maths_thinks.py
@@ -12,3 +12,12 @@ def fibonacci(n):
         n1, n2 = n2, result
         count += 1
     return result
+
+
+def fibonacci_list(n):
+    if n <= 1:
+        return n
+    fib_list = [0, 1]
+    for i in range(2, n + 1):
+        fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+    return fib_list[-1]
