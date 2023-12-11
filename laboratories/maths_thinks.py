b783db735f3737fce8b96790b8f3d345e0431636
Desolators <saschakosow@yandex.ru>
2023-12-11 11:24:24 +0300

    поменял назване переменной

--- laboratories/maths_thinks.py
+++ laboratories/maths_thinks.py
@@ -1,17 +1,20 @@
 from sys import set_int_max_str_digits as mas_str
+from sys import setrecursionlimit as mas_rec
+
+mas_rec(2 ** 10)
 mas_str(0)
 
 
 def fibonacci(n):
     if n <= 1:
         return n
-    count, n = 0, 0
+    count, n_result = 0, 0
     n1, n2 = 0, 1
     while count != n - 1:
-        n = n1 + n2
-        n1, n2 = n2, n
         count += 1
-    return n
+        n_result = n1 + n2
+        n1, n2 = n2, n_result
+    return n_result
 
 
 def fibonacci_list(n):
@@ -21,3 +24,13 @@ def fibonacci_list(n):
     for i in range(2, n + 1):
         fib_list.append(fib_list[i - 1] + fib_list[i - 2])
     return fib_list[-1]
+
+
+def fibonacci_recursion_list(n, memory):
+    if memory[n] != -1:
+        return memory[n]
+    if n <= 1:
+        return n
+    result = (fibonacci_recursion_list(n - 1, memory) + fibonacci_recursion_list(n - 2, memory))
+    memory[n] = result
+    return result
