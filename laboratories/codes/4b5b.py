be4ecd6d553942c3cc6055b087823311b5391249
Desolator <saschakosow@yandex.ru>
2024-05-13 21:48:48 +0300

    тренировка и улучшение кода

--- laboratories/codes/4b5b.py
+++ laboratories/codes/4b5b.py
@@ -64,30 +64,44 @@ def usual_code(code_input):
 dict_code = {'0000': '11110', '0001': '01001', '0010': '10100', '0011': '10101', '0100': '01010', '0101': '01011',
              '0110': '01110', '0111': '01111', '1000': '10010', '1001': '10011', '1010': '10110', '1011': '10111',
              '1100': '11010', '1101': '11011', '1110': '11100', '1111': '11101', }
-bit_code_input = '10101'
+bit_code_input = '11001010001110111011111101101111'
 while (len(bit_code_input) % 4) != 0:
     bit_code_input += '0'
 bit_code_output = b4_to_b5(bit_code_input, dict_code)
-print(bit_code_output)
+print(f'Исходная двоичная последовательность : {bit_code_input}')
+print(f'Двоичная последовательность после 4b/5b : {bit_code_output}')
+
+
 window = turtle.Screen()
-start_pos(-950, 100)
+
+start_pos(-450, 200)
 usual_code(bit_code_input)
-start_pos(-950, -100)
+
+start_pos(-450, 0)
 usual_code(bit_code_output)
-start_pos(-950, -300)
-coord_y_now = -300
+
+coord_y_now = -200
+start_pos(-450, coord_y_now)
 first_1 = 0
+bit_code_output_MLT_3 = ''
 for let in range(len(bit_code_output)):
     if bit_code_output[let] == '0':
         turtle.forward(25)
+        if coord_y_now == -175:
+            bit_code_output_MLT_3 += '+1'
+        elif coord_y_now == -200:
+            bit_code_output_MLT_3 += '0'
+        elif coord_y_now == -225:
+            bit_code_output_MLT_3 += '-1'
     elif bit_code_output[let] == '1':
-        if coord_y_now == -275:
+        if coord_y_now == -175:
             turtle.right(90)
             turtle.forward(25)
             coord_y_now += -25
             turtle.left(90)
             turtle.forward(25)
-        elif coord_y_now == -300:
+            bit_code_output_MLT_3 += '0'
+        elif coord_y_now == -200:
             if first_1 == 0:
                 first_1 = 1
                 turtle.left(90)
@@ -95,6 +109,7 @@ for let in range(len(bit_code_output)):
                 coord_y_now += 25
                 turtle.right(90)
                 turtle.forward(25)
+                bit_code_output_MLT_3 += '+1'
             elif first_1 == 1:
                 first_1 = 0
                 turtle.right(90)
@@ -102,10 +117,13 @@ for let in range(len(bit_code_output)):
                 coord_y_now += -25
                 turtle.left(90)
                 turtle.forward(25)
-        elif coord_y_now == -325:
+                bit_code_output_MLT_3 += '-1'
+        elif coord_y_now == -225:
             turtle.left(90)
             turtle.forward(25)
             coord_y_now += +25
             turtle.right(90)
             turtle.forward(25)
+            bit_code_output_MLT_3 += '0'
+print(f'Двоичная последовательность после MLT - 3 : {bit_code_output_MLT_3}')
 window.exitonclick()
